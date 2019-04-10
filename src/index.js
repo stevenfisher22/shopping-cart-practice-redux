e0803e693607e888a560140e241e16aa54aab6fe
Steven Fisher <steven.fisher22@gmail.com>
2019-04-10 16:02:42 -0500

    added base code

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// React Elements
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Redux
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux'
+
+// Components
+
+// CSS
+import './index.css'
+
+function App() {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
